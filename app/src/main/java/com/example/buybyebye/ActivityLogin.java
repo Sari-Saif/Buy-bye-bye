2b9bf2750645b1c3a1e2e1094fe7a02a3be2960d
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2024-01-10 14:20:56 +0200

    add Login_seller

--- app/src/main/java/com/example/buybyebye/ActivityLogin.java
+++ app/src/main/java/com/example/buybyebye/ActivityLogin.java
@@ -2,7 +2,9 @@ package com.example.buybyebye;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityLogin extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class ActivityLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void Login_Customer(View view) {
+        Intent i = new Intent(this , Login_seller.class);
+        startActivity(i);
+    }
+
 }
---
+++ app/src/main/java/com/example/buybyebye/Login_seller.java
@@ -0,0 +1,14 @@
+package com.example.buybyebye;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_seller extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_seller);
+    }
+}
