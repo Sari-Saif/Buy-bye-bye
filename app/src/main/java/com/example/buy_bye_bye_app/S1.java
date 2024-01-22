d961ee1b877410521fe42021085918f722ea07bc
liorvi35 <liorvi35@gmail.com>
2024-01-22 23:51:13 +0200

    updated login page customer + empty login page seller

--- app/src/main/java/com/example/buy_bye_bye_app/S1.java
+++ app/src/main/java/com/example/buy_bye_bye_app/S1.java
@@ -1,13 +1,14 @@
 package com.example.buy_bye_bye_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class C1 extends AppCompatActivity {
+public class S1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_c1);
+        setContentView(R.layout.activity_s1);
     }
 }
