6606a27f3de4d53da6bceae2734aa76b1d48d41a
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2024-01-24 00:36:42 +0200

    Add the java of c1

--- app/src/main/java/com/example/buy_bye_bye_app/C1.java
+++ app/src/main/java/com/example/buy_bye_bye_app/C1.java
@@ -1,13 +1,60 @@
 package com.example.buy_bye_bye_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class C1 extends AppCompatActivity {
+
+    //et -> EditText
+    //c-> client
+    private EditText et_emailC;
+    private EditText et_passwordC;
+    private EditText et_addressC;
+    private EditText et_visaC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c1);
+
     }
+
+    public void log_in(View view)
+    {
+        et_emailC =findViewById(R.id.InputUsername);
+        et_passwordC =findViewById(R.id.InputPassword);
+        et_addressC =findViewById(R.id.InputAddress);
+        et_visaC =findViewById(R.id.InputVisa);
+
+        //example
+        String email = et_emailC.getText().toString();
+
+        Button Register = (Button) findViewById(R.id.button);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: ADD TO DATA BASE !!
+                Intent i = new Intent(C1.this , C3.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
