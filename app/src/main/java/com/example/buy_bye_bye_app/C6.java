5a17d08b0ed02f69e0103f51a0b1fd2d7ec5afd1
SimchaTeich <sim.teich@gmail.com>
2024-01-29 00:45:30 +0200

    Connect C6 to C7

--- app/src/main/java/com/example/buy_bye_bye_app/C6.java
+++ app/src/main/java/com/example/buy_bye_bye_app/C6.java
@@ -2,7 +2,10 @@ package com.example.buy_bye_bye_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class C6 extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class C6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c6);
+
+        move_to_orders_pending_window();
+    }
+
+    private void move_to_orders_pending_window(){
+        Button orders_pending = (Button) findViewById(R.id.C6_OrdersPending_button);
+        orders_pending.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(C6.this, C7.class));
+            }
+        });
     }
 }
