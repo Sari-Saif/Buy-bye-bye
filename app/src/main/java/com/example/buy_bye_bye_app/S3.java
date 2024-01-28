7385f974d2d271f34eadb56067a57f3fdd706a51
Sari <sygw6457@gmail.com>
2024-01-28 22:45:09 +0200

    add pop_up_S3 + xml

--- app/src/main/java/com/example/buy_bye_bye_app/S3.java
+++ app/src/main/java/com/example/buy_bye_bye_app/S3.java
@@ -2,13 +2,46 @@ package com.example.buy_bye_bye_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
 
 public class S3 extends AppCompatActivity {
 
+    Button add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s3);
+
+        add = (Button) findViewById(R.id.Add_Button);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), S3_Pop_up.class);
+                startActivity(i);
+            }
+        });
+
+//        // after click on "Add" Button
+//        move_to_S3_pop_window();
+
     }
+
+    /*
+    - "Add" Button  operation
+     */
+    private void move_to_S3_pop_window() {
+        Button nextbutton = (Button) findViewById(R.id.Add_Button);
+        nextbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(S3.this, S3_Pop_up.class));
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/buy_bye_bye_app/S3_Pop_up.java
+++ app/src/main/java/com/example/buy_bye_bye_app/S3_Pop_up.java
@@ -2,13 +2,47 @@ package com.example.buy_bye_bye_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
-public class S3_Pop_up extends AppCompatActivity {
+public class S3_Pop_up extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s3_pop_up);
+
+        move_to_S3_popUP_window();
+    }
+    private void move_to_S3_popUP_window()
+    {
+        DisplayMetrics ds =new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(ds);
+
+        int width = ds.widthPixels;
+        int height = ds.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.7));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+
+        params.gravity = Gravity.CENTER;
+        params.x= 0;
+        params.y=-20;
+
+        getWindow().setAttributes(params);
+
+
+
+
+
+
+
+
     }
 }
