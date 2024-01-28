f6ea5dc733b2baac929986862640ca45b76e6cc6
SimchaTeich <sim.teich@gmail.com>
2024-01-28 23:29:00 +0200

    Connect next of C4 to C5

--- app/src/main/java/com/example/buy_bye_bye_app/C4.java
+++ app/src/main/java/com/example/buy_bye_bye_app/C4.java
@@ -2,7 +2,10 @@ package com.example.buy_bye_bye_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class C4 extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class C4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c4);
+
+        move_to_buyCart_window();
+    }
+
+    private void move_to_buyCart_window(){
+        Button nextbutton = (Button) findViewById(R.id.C4_Cart_button);
+        nextbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(C4.this, C5.class));
+            }
+        });
     }
 }
