e24edabcbc89a3d9ebfb90d0c971e3dd828006c2
Sari <sygw6457@gmail.com>
2024-01-29 02:16:31 +0200

    working on it ....

--- app/src/main/java/com/example/buy_bye_bye_app/S1.java
+++ app/src/main/java/com/example/buy_bye_bye_app/S1.java
@@ -1,5 +1,6 @@
 package com.example.buy_bye_bye_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,8 +10,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class S1 extends AppCompatActivity {
 
@@ -97,7 +101,30 @@ public class S1 extends AppCompatActivity {
 
     }
 
+    public void checkEmailExistence(String emailToCheck) {
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("user").child("customer");
+        email_check = 0;
+        userRef.orderByChild("email").equalTo(emailToCheck).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // Email already exists
+                    showErrorToast("Email already exists. Please choose a different email.");
+                    Log.d("FirebaseDebug", "Email already exists");
+                    email_check = 1;
+                } else {
+                    // Email does not exist, you can proceed
+                    Log.d("FirebaseDebug", "Email does not exist, adding new customer...");
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle errors if any
+                Log.e("FirebaseDebug", "Error checking email existence: " + databaseError.getMessage());
+            }
+        });
+    }
 
 
 }
