99503864e265a837aed8b4dec62de0b30b96fdfc
liorvi35 <liorvi35@gmail.com>
2024-02-02 16:36:54 +0200

    create new product works

--- app/src/main/java/com/example/buy_bye_bye_app/S4.java
+++ app/src/main/java/com/example/buy_bye_bye_app/S4.java
@@ -5,13 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class S4 extends AppCompatActivity {
+
+    private String store_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s4);
+
+        store_name = getIntent().getStringExtra("name");
+
+        TextView store = findViewById(R.id.textView3);
+        store.setText(store_name);
     }
 
     /**
@@ -20,6 +28,7 @@ public class S4 extends AppCompatActivity {
      */
     public void create_new_item(View view) {
         Intent i = new Intent(S4.this, S5.class);
+        i.putExtra("name", store_name);
         startActivity(i);
     }
 }
--- app/src/main/java/com/example/buy_bye_bye_app/S5.java
+++ app/src/main/java/com/example/buy_bye_bye_app/S5.java
@@ -9,17 +9,35 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class S5 extends AppCompatActivity {
 
     private final int GALLERY_REQUEST_CODE = 100;
-    ImageView img;
-    Button btn;
+
+    private String store_name;
+    private ImageView img;
+    private Button btn;
+
+    private FirebaseDatabase db;
+    private DatabaseReference ref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s5);
+
+        store_name = getIntent().getStringExtra("name");
+
+        db = FirebaseDatabase.getInstance();
+        ref = db.getReference("Stores");
 
         img = findViewById(R.id.imageView3);
         btn = findViewById(R.id.button7);
@@ -32,6 +50,25 @@ public class S5 extends AppCompatActivity {
                 startActivityForResult(Intent.createChooser(gallery, "Select Image"), GALLERY_REQUEST_CODE);
             }
         });
+    }
+
+    public void create_new_item(View view) {
+        EditText name_in = findViewById(R.id.editTextText);
+        EditText price_in = findViewById(R.id.editTextText2);
+        EditText quantity_in = findViewById(R.id.editTextText3);
+        ImageView image_in = findViewById(R.id.imageView3);
+
+        String name = name_in.getText().toString();
+        String price = price_in.getText().toString();
+        String quantity = quantity_in.getText().toString();
+
+        Product p = new Product("NULL", name, price, quantity);
+
+        ref.child(store_name).child("Products").child(name).setValue(p);
+
+        Intent i = new Intent(S5.this, S4.class);
+        i.putExtra("name", store_name);
+        startActivity(i);
     }
 
     @Override
