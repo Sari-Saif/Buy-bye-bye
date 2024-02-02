477fc4d2477bc74a7d6ed2281f7d76786d0de9ea
liorvi35 <liorvi35@gmail.com>
2024-02-02 14:49:54 +0200

    move from s4 to s5

--- app/src/main/java/com/example/buy_bye_bye_app/S4.java
+++ app/src/main/java/com/example/buy_bye_bye_app/S4.java
@@ -1,7 +1,10 @@
 package com.example.buy_bye_bye_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class S4 extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class S4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s4);
+    }
+
+    /**
+     * this function moves from S4 to S5, in order to create new item inside existing shop
+     * @param view the button
+     */
+    public void create_new_item(View view) {
+        Intent i = new Intent(S4.this, S5.class);
+        startActivity(i);
     }
 }
