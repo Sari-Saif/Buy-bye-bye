138329a6adbb47a8a6fe6486979f7c5425122cab
liorvi35 <liorvi35@gmail.com>
2024-02-02 15:43:23 +0200

    choose image works

--- app/src/main/java/com/example/buy_bye_bye_app/S5.java
+++ app/src/main/java/com/example/buy_bye_bye_app/S5.java
@@ -1,14 +1,46 @@
 package com.example.buy_bye_bye_app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class S5 extends AppCompatActivity {
 
+    private final int GALLERY_REQUEST_CODE = 100;
+    ImageView img;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s5);
+
+        img = findViewById(R.id.imageView3);
+        btn = findViewById(R.id.button7);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery = new Intent(Intent.ACTION_GET_CONTENT);
+                gallery.setType("image/*");
+                startActivityForResult(Intent.createChooser(gallery, "Select Image"), GALLERY_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQUEST_CODE && data != null) {
+            Uri uri =   data.getData();
+            img.setImageURI(uri);
+        }
     }
 }
