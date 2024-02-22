c5a1ddb79e84bd33df3efc3b56f2c714afe063bf
SimchaTeich <sim.teich@gmail.com>
2024-02-22 10:51:19 +0200

    fix the bg

--- app/src/main/java/com/example/buy_bye_bye_app/C5.java
+++ app/src/main/java/com/example/buy_bye_bye_app/C5.java
@@ -25,28 +25,23 @@ import java.util.ArrayList;
 import java.util.EventListener;
 
 public class C5 extends AppCompatActivity {
-
     RecyclerView recyclerView;
     ArrayList<ProductInCart> ProductsInCartList;
     DatabaseReference databaseReference;
-
-    //bag?
     ValueEventListener event;
-    ValueEventListener event2;
-
     ProductInCartAdapter adapter;
-
     FirebaseDatabase database;
     FirebaseUser user;
     String userEmail;
     String userEmailWithoutDot;
-
     int totalCartPrice;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c5);
+
 
         // add the current store name
         Intent intent = getIntent();
@@ -54,61 +49,32 @@ public class C5 extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.C5_Store_Name_textView);
         textView.setText(store_name);
 
+
+        // update in real time for total cart price
         totalCartPrice = 0;
 
-        //bag?
-        event = new ValueEventListener() {
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String CustomerName = snapshot.child("CustomerName").getValue(String.class);
-                String OrderID = snapshot.child("OrderID").getValue(String.class);
-                String StoreName = snapshot.child("StoreName").getValue(String.class);
 
-                // copy them into the Active
-                database.getReference("Orders").child("Active").child(OrderID).child("CustomerName").setValue(CustomerName);
-                database.getReference("Orders").child("Active").child(OrderID).child("OrderID").setValue(OrderID);
-                database.getReference("Orders").child("Active").child(OrderID).child("StoreName").setValue(StoreName);
-                for (ProductInCart p : ProductsInCartList){
-                    database.getReference("Orders").child("Active").child(OrderID).child("Products").child(p.getName()).child("Name").setValue(p.getName());
-                    database.getReference("Orders").child("Active").child(OrderID).child("Products").child(p.getName()).child("Price").setValue(p.getPrice());
-                    database.getReference("Orders").child("Active").child(OrderID).child("Products").child(p.getName()).child("Amount").setValue(p.getAmount());
-                }
-
-                // delete the cart from Carts.
-                database.getReference("Orders").child("Carts").child(userEmailWithoutDot).removeValue();
-
-                // back to the list of stores
-                databaseReference.removeEventListener(event2);
-                startActivity(new Intent(C5.this, C3.class));
-                finish();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        };
-
-        // save the userEmail
+        // save the user info (help to get into the correct cart)
         database = FirebaseDatabase.getInstance();
         user = FirebaseAuth.getInstance().getCurrentUser();
         userEmail = user.getEmail().toString();
         userEmailWithoutDot = userEmail.replace(".", "_");
 
+
+        // handle the list of products in cart
         recyclerView = (RecyclerView)findViewById(R.id.C5_recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         ProductsInCartList = new ArrayList<>();
-
         adapter = new ProductInCartAdapter(this, ProductsInCartList);
         recyclerView.setAdapter(adapter);
 
+
+        // save reference for products in the current user cart.
         databaseReference = FirebaseDatabase.getInstance().getReference("Orders").child("Carts").child(userEmailWithoutDot).child("Products");
 
 
-        //bag2?
-        event2 = new ValueEventListener() {
+        // event save in variable to remove it from listener when exit the activity.
+        event = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 ProductsInCartList.clear();
@@ -127,10 +93,9 @@ public class C5 extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
         };
-        databaseReference.addValueEventListener(event2);
+        databaseReference.addValueEventListener(event);
 
 
         // buttons
@@ -139,6 +104,13 @@ public class C5 extends AppCompatActivity {
     }
 
 
+    /*
+    * function for buy button
+    * copy the cart from Carts branch into Active branch (firebase)
+    * and then delete the cart.
+    * after user buy something he get out from the store.
+    * and back to list of store.
+    * */
     private void button_buy(){
         Button buy = (Button) findViewById(R.id.C5_Buy_button);
         buy.setOnClickListener(new View.OnClickListener() {
@@ -151,52 +123,56 @@ public class C5 extends AppCompatActivity {
                     // get the cart values
                     FirebaseDatabase database = FirebaseDatabase.getInstance();
                     DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Orders").child("Carts").child(userEmailWithoutDot);
-//                    event = new ValueEventListener() {
-//                        @Override
-//                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                            // get the values from the  Cart
-//                            String CustomerName = snapshot.child("CustomerName").getValue(String.class);
-//                            String OrderID = snapshot.child("OrderID").getValue(String.class);
-//                            String StoreName = snapshot.child("StoreName").getValue(String.class);
-//
-//                            // copy them into the Active
-//                            database.getReference("Orders").child("Active").child(OrderID).child("CustomerName").setValue(CustomerName);
-//                            database.getReference("Orders").child("Active").child(OrderID).child("OrderID").setValue(OrderID);
-//                            database.getReference("Orders").child("Active").child(OrderID).child("StoreName").setValue(StoreName);
-//                            for (ProductInCart p : ProductsInCartList){
-//                                database.getReference("Orders").child("Active").child(OrderID).child("Products").child(p.getName()).child("Name").setValue(p.getName());
-//                                database.getReference("Orders").child("Active").child(OrderID).child("Products").child(p.getName()).child("Price").setValue(p.getPrice());
-//                                database.getReference("Orders").child("Active").child(OrderID).child("Products").child(p.getName()).child("Amount").setValue(p.getAmount());
-//                            }
-//
-//                            // delete the cart from Carts.
-//                            database.getReference("Orders").child("Carts").child(userEmailWithoutDot).removeValue();
-//
-//                            // back to the list of stores
-//                            //startActivity(new Intent(C5.this, C3.class));
-//                            //finish();
-//                        }
-//
-//                        @Override
-//                        public void onCancelled(@NonNull DatabaseError error) {
-//
-//                        }
-//                    };
-                    ref.addListenerForSingleValueEvent(event);
+                    ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            // get the values from the  Cart
+                            String CustomerName = snapshot.child("CustomerName").getValue(String.class);
+                            String OrderID = snapshot.child("OrderID").getValue(String.class);
+                            String StoreName = snapshot.child("StoreName").getValue(String.class);
+
+                            // copy them into the Active
+                            database.getReference("Orders").child("Active").child(OrderID).child("CustomerName").setValue(CustomerName);
+                            database.getReference("Orders").child("Active").child(OrderID).child("OrderID").setValue(OrderID);
+                            database.getReference("Orders").child("Active").child(OrderID).child("StoreName").setValue(StoreName);
+                            for (ProductInCart p : ProductsInCartList){
+                                database.getReference("Orders").child("Active").child(OrderID).child("Products").child(p.getName()).child("Name").setValue(p.getName());
+                                database.getReference("Orders").child("Active").child(OrderID).child("Products").child(p.getName()).child("Price").setValue(p.getPrice());
+                                database.getReference("Orders").child("Active").child(OrderID).child("Products").child(p.getName()).child("Amount").setValue(p.getAmount());
+                            }
+
+                            // delete the cart from Carts.
+                            database.getReference("Orders").child("Carts").child(userEmailWithoutDot).removeValue();
+
+                            // back to the list of stores
+                            databaseReference.removeEventListener(event);
+                            startActivity(new Intent(C5.this, C3.class));
+                            finish();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                        }
+                    });
                 }
             }
         });
     }
+
+
+    /*
+    * function for back button.
+    * remove the db event listener and destroy the activity
+    * */
     private void button_back()
     {
         Button button = (Button) findViewById(R.id.C5_Back_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                databaseReference.removeEventListener(event2);
+                databaseReference.removeEventListener(event);
                 finish();
             }
         });
-
     }
 }
